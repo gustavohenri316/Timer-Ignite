1132f41a1e21fada9497c52d97f859cab664daa1
gustavohenri316 <gustavohenri316@gmail.com>
2022-08-04 15:27:50 -0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
-
-
